b84d61584840a2fe73b71757df574592c9e2bed0
wymek <wymek@users.noreply.github.com>
2020-12-18 16:05:26 +0100

    + some pages

---
+++ src/pages/ChartPage/ChartPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ChartPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ChartPage;
---
+++ src/pages/DoctorPage/DoctorPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DoctorPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default DoctorPage;
