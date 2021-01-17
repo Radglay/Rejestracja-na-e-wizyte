eb4314a8c5b55754f0fbae6a308fe35159ff4555
Radglay <32621646+Radglay@users.noreply.github.com>
2021-01-17 17:00:51 +0100

    +czysta baza

--- server/src/main/java/projekt/ewizyta/controller/WizytaController.java
+++ server/src/main/java/projekt/ewizyta/controller/WizytaController.java
@@ -1,10 +1,7 @@
 package projekt.ewizyta.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import projekt.ewizyta.model.Doctor;
 import projekt.ewizyta.model.User;
 import projekt.ewizyta.model.Wizyta;
@@ -86,5 +83,7 @@ public class WizytaController {
 
             return wizytaObj;
         }
-
+//
+//
+//        @DeleteMapping("/api/wizyta_delete")
 }
